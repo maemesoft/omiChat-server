a5a4ce7fab19f6c4b0b102166ae856d0b62a5bc4
maemesoft <maemesoft@naver.com>
2020-10-26 10:33:11 +0000

    Add resolvers for auth

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,6 +1,7 @@
+import AuthResolver from './auth';
 import UserResolver from './user';
 
 // If delete any type in resolvers const, cause type error
-const resolvers: any = [UserResolver];
+const resolvers: any = [UserResolver, AuthResolver];
 
 export default resolvers;
