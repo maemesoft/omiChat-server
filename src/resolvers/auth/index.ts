628b6890bf94f366f5f56c3792c0b02d3c0ef7c0
maemesoft <maemesoft@naver.com>
2020-10-31 18:17:56 +0000

    Update getLogin for token

--- src/resolvers/auth/index.ts
+++ src/resolvers/auth/index.ts
@@ -15,21 +15,29 @@ export default class AuthResolver {
 
     /**
      * @description 이메일을 이용하여 로그인을 진행합니다.
-     * @returns {User | Error}
+     * @returns {String | Error}
      * @param email 로그인에 사용할 이메일을 입력합니다.
      * @param password 로그인에 사용할 비밀번호를 입력합니다.
      */
-    @Query((type) => User)
+    @Query((type) => String)
     public async getLogin(
         @Arg('email') email: string,
         @Arg('password') password: string
-    ): Promise<User | Error> {
-        return await emailLogin(email, password);
+    ): Promise<String | Error> {
+        const result = await emailLogin(email, password);
+
+        if (result instanceof Error) {
+            return result;
+        }
+
+        // result에 포함된 id를 이용하여 Token을 발급합니다
+        const token = tokenIssue(result.id);
+        return token;
     }
 
     /**
      * @description 이메일을 이용하여 회원가입을 진행합니다. 만약 사용하고자 하는 이메일, 닉네임이 존재할 경우 Error를 반환합니다
-     * @returns {User | Error}
+     * @returns {String | Error}
      * @param email 회원가입시 사용할 이메일을 입력합니다
      * @param password 회원가입시 사용할 비밀번호를 입력합니다
      * @param nickname 회원가입시 사용할 닉네임을 입력합니다
